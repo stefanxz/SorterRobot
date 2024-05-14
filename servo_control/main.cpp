5863463b3fcffbde864106217b7c56b6d44afed7
Stefan Avram <stefanavram55@gmail.com>
2024-05-14 19:28:28 +0200

    Created a variant of code that works for the pitson setup that uses a 360° servomotor

--- servo_control/main.cpp
+++ servo_control/main.cpp
@@ -4,20 +4,6 @@
 int main() {
     int pin = 11; // Physical pin 11 corresponds to WiringPi pin 0
     Servo servo(pin);
-
-    for(;;){
-        for (int i = 0; i <= 180; i+=45)
-        {
-            servo.moveToAngle(i);
-        }
-
-        for (int i = 135; i >= 0; i-=45)
-        {
-            servo.moveToAngle(i);
-        }
-    }
-    
-    
-
+    servo.movePiston(1000000); // 1 Second in microseconds.
     return 0;
 }
