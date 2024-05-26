4d8584966aaa3ae1034af45a86258fabf4e35c8d
Stefan Avram <stefanavram55@gmail.com>
2024-05-26 15:30:25 +0200

    Testing for motor control and fixed issues with constructor of MotorController.cpp.

--- raspberry_pi/main_controller/main.cpp
+++ raspberry_pi/main_controller/main.cpp
@@ -1,22 +1,59 @@
-//
-// Created by Stefan on 24.05.2024.
-//
-#include <iostream>
-#include "../util/display_control/DisplayController.h"
-#include "../util/light_sensor/ADCReader.h"
 #include "../util/motor_control/MotorController.h"
-#include "../util/servo_control/Servo.h"
-#include <wiringPi.h>
-#include <softPwm.h>
-#include <wiringPiI2C.h>
-#include <unistd.h>
+#include "../util/servo_control//Servo.h"
+#include "../util/light_sensor/ADCReader.h"
+#include "../util/display_control/DisplayController.h"
+
+#include <iostream>
 
 using namespace std;
 
 int main() {
+    char command;
+    bool direction = true; // true = forward, false = backward
+    int speed = 25; // default speed
 
-    Servo servoController(11);
-    servoController.movePiston();
+    MotorController motor(24, 23, 25);
+    motor.setup();
 
-    return 0;
+    cout << "The default speed & direction of motor is LOW & Forward....." << endl;
+    cout << "r-run s-stop f-forward b-backward l-low m-medium h-high e-exit" << endl;
+
+    while (true) {
+        cin >> command;
+
+        switch (command) {
+            case 'r':
+                motor.run(direction, speed);
+                break;
+            case 's':
+                motor.stop();
+                break;
+            case 'f':
+                direction = true;
+                motor.run(direction, speed);
+                break;
+            case 'b':
+                direction = false;
+                motor.run(direction, speed);
+                break;
+            case 'l':
+                speed = 25;
+                motor.changeSpeed(speed);
+                break;
+            case 'm':
+                speed = 50;
+                motor.changeSpeed(speed);
+                break;
+            case 'h':
+                speed = 75;
+                motor.changeSpeed(speed);
+                break;
+            case 'e':
+                motor.stop();
+                return 0;
+            default:
+                cout << "<<< wrong data >>>" << endl;
+                cout << "please enter the defined data to continue....." << endl;
+        }
+    }
 }
